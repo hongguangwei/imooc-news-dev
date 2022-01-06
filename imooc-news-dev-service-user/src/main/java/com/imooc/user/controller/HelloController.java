d6f54766256c7982a1df66753a8000e57719978d
hgw <honggw@izkml.com>
2022-01-06 08:44:27 +0800

    hello

---
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.imooc.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author honggw
+ * @create 2022-01-05 17:26
+ * @描述：
+ */
+@RestController
+public class HelloController {
+
+
+
+}
